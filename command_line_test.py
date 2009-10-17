cb9873d34b013d389e92642ff1487bba4f68331c
sterlingsomers@gmail.com <sterlingsomers@gmail.com@60cf8ed4-aec4-11de-b3ee-c79121bffc40>
2009-10-17 15:35:00 +0000

    Some more basics for the command line idea

--- command_line_test.py
+++ command_line_test.py
@@ -7,7 +7,10 @@
 
 import cmd
 import string, sys
-import os
+
+
+from make_project import *
+from inventory_tools import *
 
 class LakeCmdInterface(cmd.Cmd):
     
@@ -20,18 +23,18 @@ class LakeCmdInterface(cmd.Cmd):
         return True
     
 
-    def do_inventory(self, *args):
-        #args = [] of arguments
+    def do_inventory(self, args):
         #all objects are really <obj_objectName> folders.  i.e. <obj_basicAgent>
-        currentpath = os.getcwd()
-        inventory = currentpath + "\inventory"
-        
-        for things in os.listdir(inventory):
-            if os.path.isdir(os.path.join(inventory,things)):
-                #then it's a folder, not a file
-                if (things[0:4] == "obj_"):
-                    #then it begin's with obj_ and is an object
-                    print(things[4:])#lists the objects
+        list_inventory()
+##        currentpath = os.getcwd()
+##        inventory = os.path.join(currentpath,"inventory")#currentpath + "\inventory"
+##        
+##        for things in os.listdir(inventory):
+##            if os.path.isdir(os.path.join(inventory,things)):
+##                #then it's a folder, not a file
+##                if (things[0:4] == "obj_"):
+##                    #then it begin's with obj_ and is an object
+##                    print(things[4:])#lists the objects
                 
         
 
@@ -44,9 +47,23 @@ class LakeCmdInterface(cmd.Cmd):
         print("syntax: quit")
         print("-- terminates the application")
 
-    def do_create(self, *args):
-        #args = [] of arguments
+    def do_create(self, args):
+        args = args.split()
+        if not args[1] == "called":
+            return print("Proper use: create", args[0], "called <a name>")
+        if len(args) > 3:
+            return print("Too many argument")
         #create <obj_> called <name>
+        #create project called <name>
+
+        #project case
+        if args[0] == "project":
+            create_project(args[2])
+        
+        elif check_inventory(args[0]): #check to see if there is a template in the inventory, True/False
+            print("matches..")
+            
+        
         
 
     def help_create(self):
